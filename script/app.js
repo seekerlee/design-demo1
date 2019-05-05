cec5a9c5c481be993e9fcea06bf3fd2977b94b25
Li Shi <lishi1608@gmail.com>
2019-05-05 22:42:07 +0800

    step 3: 使用 pace.js 增加载入进度条

--- script/app.js
+++ script/app.js
@@ -1,1 +1,12 @@
 const isWide = window.innerWidth > 768
+
+// enterHome 函数包含了页面载入后要执行的操作
+function enterHome() {
+  // 给相关元素增加 animation 类以执行动画，在 CSS 中给 animation 类定义了动画
+  document.querySelectorAll("header h1 span").forEach(e => {
+    e.classList.add("animation")
+  })
+  document.querySelector("header p.mysubtitle").classList.add("animation")
+}
+// 在载入完成后执行 enterHome 函数
+Pace.once("done", enterHome)
